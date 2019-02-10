c90f9ca1d21569ebe1b6d256729def4335d2203b
Maailmavallutaja <46933175+Maailmavallutaja@users.noreply.github.com>
2019-02-10 18:31:40 +0200

    recipes
    added addrecipe php.
    modified dunno.html to index.php
    added link to addrecipe.php
    created database for recipies ( only local currently)
    put css to dunno.css
    added fonts

--- main.js
+++ main.js
@@ -1,51 +1,64 @@
+
+$(function() {
+    $('.jqueryOptions').hide();
+  
+    $('#choose').change(function () {
+      $('.jqueryOptions').slideUp();
+      $('.jqueryOptions').removeClass('current-opt');
+      $("." + $(this).val()).slideDown();
+      $("." + $(this).val()).addClass('current-opt');
+    });
+  });
+
+
 //save form input
-var localIngredients = []; // declare new array
-var addedIngredient = document.getElementById('addedIngredient').innerHTML; // get ingredient from form input
+// var localIngredients = []; // declare new array
+// var addedIngredient = document.getElementById('addedIngredient').innerHTML; // get ingredient from form input
 
-var addButton = document.getElementById('saveIngredientButton');// declare Addbutton
-var removeButton = document.getElementById('removeIngredientButton');
+// var addButton = document.getElementById('saveIngredientButton');// declare Addbutton
+// var removeButton = document.getElementById('removeIngredientButton');
 
 
-//to display new added ingredient
+// //to display new added ingredient
 
-function displayIngredient(localIngredient) {
+// function displayIngredient(localIngredient) {
     
-    var displayLocalIngredients = document.createElement('p'); //create the element
-    displayLocalIngredients.innerHTML = localIngredient; //declare the content of the element
-    document.getElementById('ingredientsDisplay').appendChild(displayLocalIngredients); // add created HTML element to document
-    var createRemoveButton = document.createElement()
-}
+//     var displayLocalIngredients = document.createElement('p'); //create the element
+//     displayLocalIngredients.innerHTML = localIngredient; //declare the content of the element
+//     document.getElementById('ingredientsDisplay').appendChild(displayLocalIngredients); // add created HTML element to document
+//     var createRemoveButton = document.createElement()
+// }
 
-function getIngredientId(){
+// function getIngredientId(){
     
-}
-//fuction to add ingredient to localIngredients array
-addButton.onclick = function addIngredient(ingredient) {
-    localIngredients.push(ingredient); // add ingredient to array (array push method)
-    displayIngredient(ingredient); // display the ingredient
-    document.getElementById('addedIngredient').reset(); // resets the input field
-};
+// }
+// //fuction to add ingredient to localIngredients array
+// addButton.onclick = function addIngredient(ingredient) {
+//     localIngredients.push(ingredient); // add ingredient to array (array push method)
+//     displayIngredient(ingredient); // display the ingredient
+//     document.getElementById('addedIngredient').reset(); // resets the input field
+// };
 
-//fuction to add ingredient to localIngredients array
-addButton.onclick = function addIngredient(ingredient) {
-    localIngredients.push(ingredient); // add ingredient to array (array push method)
-    displayIngredient(ingredient); // display the ingredient
-    document.getElementById('addedIngredient').reset(); // resets the input field
-};
-// Should use form submit() function instead?
-//https://www.w3schools.com/jsref/met_form_submit.asp
+// //fuction to add ingredient to localIngredients array
+// addButton.onclick = function addIngredient(ingredient) {
+//     localIngredients.push(ingredient); // add ingredient to array (array push method)
+//     displayIngredient(ingredient); // display the ingredient
+//     document.getElementById('addedIngredient').reset(); // resets the input field
+// };
+// // Should use form submit() function instead?
+// //https://www.w3schools.com/jsref/met_form_submit.asp
 
 
-//function to create removing button
-removeButton.onclick = function removeIngredient() {
-    localIngredients.pop();
-};
+// //function to create removing button
+// removeButton.onclick = function removeIngredient() {
+//     localIngredients.pop();
+// };
 
-class Ingredient{
-    constructor(name){
-        this.name = name;
+// class Ingredient{
+//     constructor(name){
+//         this.name = name;
         
-    }
+//     }
     //to display new added ingredient
     
 }
